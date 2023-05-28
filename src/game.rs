3aae086d3a83fa29748965c0ccf4de1fba9a48de
jel <25802745+jelni@users.noreply.github.com>
2023-05-28 21:21:10 +0200

    code cleanup

---
+++ src/game.rs
@@ -0,0 +1,104 @@
+use macroquad::prelude::{KeyCode, MouseButton, GRAY, WHITE};
+use macroquad::{input, shapes, text, time, window};
+
+use crate::board::Board;
+
+const GLIDER_GUN: [&[usize]; 9] = [
+    &[24],
+    &[22, 24],
+    &[12, 13, 20, 21, 34, 35],
+    &[11, 15, 20, 21, 34, 35],
+    &[0, 1, 10, 16, 20, 21],
+    &[0, 1, 10, 14, 16, 17, 22, 24],
+    &[10, 16, 24],
+    &[11, 15],
+    &[12, 13],
+];
+
+pub struct Game {
+    paused: bool,
+    scale: usize,
+    board: Board,
+}
+
+impl Game {
+    pub fn new() -> Self {
+        let mut board = Board::new((64, 64));
+
+        for (y, row) in GLIDER_GUN.into_iter().enumerate() {
+            for &x in row {
+                board.set_cell((x, y), true);
+            }
+        }
+
+        Self {
+            paused: true,
+            scale: 4,
+            board,
+        }
+    }
+
+    pub async fn game_loop(mut self) {
+        loop {
+            self.simulate();
+            self.handle_input();
+            self.draw();
+            window::next_frame().await;
+        }
+    }
+
+    fn simulate(&mut self) {
+        if !self.paused {
+            self.board = self.board.next_state(
+                #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+                (
+                    (window::screen_width() as usize / self.scale).next_power_of_two(),
+                    (window::screen_height() as usize / self.scale).next_power_of_two(),
+                ),
+            );
+        }
+    }
+
+    fn handle_input(&mut self) {
+        if input::is_key_pressed(KeyCode::Space) {
+            self.paused = !self.paused;
+        }
+
+        let scroll = input::mouse_wheel().1;
+        if scroll != 0. {
+            self.scale = if scroll.is_sign_positive() {
+                (self.scale * 2).min(16)
+            } else {
+                (self.scale / 2).max(1)
+            };
+        }
+
+        let left_button = input::is_mouse_button_down(MouseButton::Left);
+        let right_button = input::is_mouse_button_down(MouseButton::Right);
+
+        if left_button || right_button {
+            let position = input::mouse_position();
+            #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+            let position = (
+                position.0 as usize / self.scale,
+                position.1 as usize / self.scale,
+            );
+            self.board.set_cell(position, left_button);
+        }
+    }
+
+    fn draw(&self) {
+        for (x, y) in self.board.cells() {
+            #[allow(clippy::cast_precision_loss)]
+            shapes::draw_rectangle(
+                (self.scale * x) as f32,
+                (self.scale * y) as f32,
+                self.scale as f32,
+                self.scale as f32,
+                WHITE,
+            );
+        }
+
+        text::draw_text(&time::get_fps().to_string(), 0., 8., 16., GRAY);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,21 @@
 #![warn(clippy::pedantic)]
 
-use game_of_life::Board;
-use macroquad::prelude::{KeyCode, MouseButton, BLACK, GRAY, WHITE};
+use game::Game;
 use macroquad::window::Conf;
-use macroquad::{input, shapes, text, time, window, Window};
+use macroquad::Window;
 
-mod game_of_life;
+mod board;
+mod game;
 mod quad_tree;
-
-const GLIDER_GUN: [&[usize]; 9] = [
-    &[24],
-    &[22, 24],
-    &[12, 13, 20, 21, 34, 35],
-    &[11, 15, 20, 21, 34, 35],
-    &[0, 1, 10, 16, 20, 21],
-    &[0, 1, 10, 14, 16, 17, 22, 24],
-    &[10, 16, 24],
-    &[11, 15],
-    &[12, 13],
-];
 
 fn main() {
     Window::from_config(
         Conf {
-            window_title: "game of life".into(),
+            window_title: "Conway's Game of Life".into(),
             window_width: 512,
             window_height: 512,
             ..Default::default()
         },
-        run(),
+        Game::new().game_loop(),
     );
-}
-
-async fn run() {
-    let mut board = Board::new((64, 64));
-
-    for (y, row) in GLIDER_GUN.into_iter().enumerate() {
-        for &x in row {
-            board.set_cell((x, y), true);
-        }
-    }
-
-    let mut paused = false;
-    let mut scale = 4;
-
-    loop {
-        if !paused {
-            board = board.next_state(
-                #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
-                (
-                    (window::screen_width() as usize / scale).next_power_of_two(),
-                    (window::screen_height() as usize / scale).next_power_of_two(),
-                ),
-            );
-        }
-
-        if input::is_key_pressed(KeyCode::Space) {
-            paused = !paused;
-        }
-
-        let scroll = input::mouse_wheel().1;
-        if scroll != 0. {
-            scale = if scroll.is_sign_positive() {
-                (scale * 2).min(16)
-            } else {
-                (scale / 2).max(1)
-            };
-        }
-
-        let left_button = input::is_mouse_button_down(MouseButton::Left);
-        let right_button = input::is_mouse_button_down(MouseButton::Right);
-
-        if left_button || right_button {
-            let position = input::mouse_position();
-            #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
-            let position = (position.0 as usize / scale, position.1 as usize / scale);
-            board.set_cell(position, left_button);
-        }
-
-        window::clear_background(BLACK);
-
-        for (x, y) in board.cells() {
-            #[allow(clippy::cast_precision_loss)]
-            shapes::draw_rectangle(
-                (scale * x) as f32,
-                (scale * y) as f32,
-                scale as f32,
-                scale as f32,
-                WHITE,
-            );
-        }
-
-        text::draw_text(&time::get_fps().to_string(), 0., 8., 16., GRAY);
-
-        window::next_frame().await;
-    }
 }
