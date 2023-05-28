9a9a3a59b2e472c3c293a5946c3a3df01b881668
jel <25802745+jelni@users.noreply.github.com>
2023-05-28 20:20:45 +0200

    allow scaling with mouse wheel

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@
 
 use game_of_life::Board;
 use macroquad::prelude::{BLACK, GRAY, WHITE};
-use macroquad::{shapes, text, time, window};
+use macroquad::window::Conf;
+use macroquad::{input, shapes, text, time, window, Window};
 
 mod game_of_life;
 mod quad_tree;
@@ -19,11 +20,19 @@ const GLIDER_GUN: [&[usize]; 9] = [
     &[12, 13],
 ];
 
-#[macroquad::main("game of life")]
-async fn main() {
-    window::request_new_screen_size(512., 512.);
-    window::next_frame().await;
+fn main() {
+    Window::from_config(
+        Conf {
+            window_title: "game of life".into(),
+            window_width: 512,
+            window_height: 512,
+            ..Default::default()
+        },
+        run(),
+    );
+}
 
+async fn run() {
     let mut board = Board::new((64, 64));
 
     for (y, row) in GLIDER_GUN.into_iter().enumerate() {
@@ -32,19 +41,37 @@ async fn main() {
         }
     }
 
+    let mut scale = 4;
+
     loop {
         #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
         {
             board = board.next_state((
-                (window::screen_width() as usize / 4).next_power_of_two(),
-                (window::screen_height() as usize / 4).next_power_of_two(),
+                (window::screen_width() as usize / scale).next_power_of_two(),
+                (window::screen_height() as usize / scale).next_power_of_two(),
             ));
         }
+
+        let scroll = input::mouse_wheel().1;
+        if scroll != 0. {
+            scale = if scroll.is_sign_positive() {
+                (scale * 2).min(16)
+            } else {
+                (scale / 2).max(1)
+            };
+        }
+
         window::clear_background(BLACK);
 
         for (x, y) in board.cells() {
             #[allow(clippy::cast_precision_loss)]
-            shapes::draw_rectangle((4 * x) as f32, (4 * y) as f32, 4., 4., WHITE);
+            shapes::draw_rectangle(
+                (scale * x) as f32,
+                (scale * y) as f32,
+                scale as f32,
+                scale as f32,
+                WHITE,
+            );
         }
 
         text::draw_text(&time::get_fps().to_string(), 0., 8., 16., GRAY);
